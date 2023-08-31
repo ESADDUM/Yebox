104eff8977dacd137905c362df1ed60f7f959259
Eduardo Stephan Addum <ART0060362@aluno.firjansenaisesi.com.br>
2023-08-31 12:10:47 -0300

    update

--- js/main.js
+++ js/main.js
@@ -10,7 +10,12 @@ let Start = setInterval(()=>{
     // Add your methods for Sanddox here
     YeEngine.AlterThemeSystem(YeEngine.Theme.DarkTheme)
     YeEngine.AlterTheme(YeEngine.Theme.SkyTheme)
+    YeEngine.Questions_save[0]
 
     //End Your methods Here
     clearInterval(Start) //Dont erase this line
+},400)
+
+let update = setInterval()=> {
+YeEngine.ModifyContentClass(document.getElementById("quest1"."Olá Mundo")
 },400)
